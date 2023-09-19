10d809dae07e6f8089aac0613182f97a4485f232
ollsave27 <olegsav1076@gmail.com>
2023-09-19 15:21:54 +0300

    первая задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	const d = b ** 2 - 4 * a * c;
+
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		const root = -b / (2 * a);
+		arr.push(root);
+	} else {
+		const root1 = (-b + Math.sqrt(d)) / (2 * a);
+		const root2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(root1, root2);
+	}
+
+	return arr;
 }
+
+const roots = solveEquation(1, -3, 2);
+console.log(roots);
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
-}
+ }
