1720f2ea6d721fe2422ba56e66fe93067352fc5d
ollsave27 <olegsav1076@gmail.com>
2023-10-08 21:30:30 +0300

    Шестое задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,62 @@
-﻿
+﻿function parseCount (num) {
+    let num1 = Number.parseFloat(num);
+    if(Number.isNaN(num1)) {
+        throw new Error("Невалидное значение");
+    }
+    else {
+        return num1;
+    };
+}
+
+function validateCount(num) {
+    try {
+        return parseCount(num);
+    }
+    catch(err) {
+        return err;
+    };
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if(((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a)) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        else {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+    };
+}   
+
+get perimeter() {
+    let p = (this.a + this.b + this.c);
+    return p;
+}
+
+get area () {
+    let p = (this.a + this.b + this.c) / 2;
+    let ar = Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+    return ar;
+    };
+}
+
+function getTriangle (a, b, c) {
+    try {
+        let trg = new Triangle(a, b, c);
+        return trg;
+    }
+    catch (error) {
+        let err = {
+            get perimeter() {
+               return "Ошибка! Треугольник не существует";
+            },
+            get area() {
+               return "Ошибка! Треугольник не существует";
+            }
+        }
+        return err;
+    }
+    finally {
+    };
+}    
